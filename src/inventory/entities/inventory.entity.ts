b81b9788af7924cd9aa5db53e0ab20ef3ae33d64
developedBySJ <developedbysj@gmail.com>
2021-06-09 16:02:29 +0530

    Add inventory entity

--- src/inventory/entities/inventory.entity.ts
+++ src/inventory/entities/inventory.entity.ts
@@ -1,1 +1,41 @@
-export class Inventory {}
+import { UserEntity } from 'src/users/entities/user.entity'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+
+@Entity('inventory')
+export class Inventory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column()
+  name: string
+
+  @Column({ name: 'available_stock' })
+  availableStock: number
+
+  @Column({ nullable: true })
+  image?: string
+
+  @Column()
+  tags: string[]
+
+  @ManyToOne(() => UserEntity, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'created_by' })
+  createdBy?: UserEntity
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date
+}
