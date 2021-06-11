a5c276cf4672f156cba5e8fdb5d13c48027e62ad
developedBySJ <developedbysj@gmail.com>
2021-06-11 21:37:23 +0530

    Update inventory controller

--- src/inventory/inventory.controller.ts
+++ src/inventory/inventory.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common'
 import { InventoryService } from './inventory.service'
 import { CreateInventoryDto } from './dto/create-inventory.dto'
@@ -26,20 +27,20 @@ export class InventoryController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.inventoryService.findOne(+id)
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.inventoryService.findOne(id)
   }
 
   @Patch(':id')
   update(
-    @Param('id') id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateInventoryDto: UpdateInventoryDto,
   ) {
     return this.inventoryService.update(+id, updateInventoryDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.inventoryService.remove(+id)
   }
 }
