5bf008185dacea0b09a283f28a6028de2c16626f
developedBySJ <developedbysj@gmail.com>
2021-06-17 20:51:23 +0530

    Add create menu item dto

--- src/menu-items/dto/create-menu-item.dto.ts
+++ src/menu-items/dto/create-menu-item.dto.ts
@@ -1,1 +1,47 @@
-export class CreateMenuItemDto {}
+import {
+  IsBoolean,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  Length,
+  Max,
+  Min,
+} from 'class-validator'
+
+export class CreateMenuItemDto {
+  @Length(3, 50)
+  title: string
+
+  @Length(3, 100)
+  @IsOptional()
+  subTitle?: string
+
+  @IsString({ each: true })
+  @IsOptional()
+  images: string[]
+
+  @IsBoolean()
+  isAvailable: boolean = false
+
+  @IsBoolean()
+  isVeg: boolean = false
+
+  @IsPositive()
+  price: number
+
+  @Min(0)
+  @Max(99)
+  discount: number = 0
+
+  @Length(3, 1000)
+  @IsOptional()
+  description?: string
+
+  @IsPositive()
+  @IsOptional()
+  prepTime?: number
+
+  @IsUUID()
+  menu: string
+}
