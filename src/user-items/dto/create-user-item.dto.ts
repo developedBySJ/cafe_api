480809a8defbec282f8c3e104fa3ae1d302d2f40
developedBySJ <developedbysj@gmail.com>
2021-06-19 15:58:20 +0530

    Add create user item dto

--- src/user-items/dto/create-user-item.dto.ts
+++ src/user-items/dto/create-user-item.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserItemDto {}
+import { IsOptional, IsPositive, IsUUID, Max, Min } from 'class-validator'
+
+export class CreateUserItemDto {
+  @IsUUID()
+  menuItem: string
+
+  @Min(0)
+  @Max(10)
+  @IsOptional()
+  qty?: number
+}
