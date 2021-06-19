c7da52f8edaeb6a5158007c1b463357b127995c6
developedBySJ <developedbysj@gmail.com>
2021-06-19 17:17:12 +0530

    add create review dto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,21 @@
-export class CreateReviewDto {}
+import { IsOptional, IsUUID, Length, Max, Min } from 'class-validator'
+import { UserEntity } from 'src/users/entities/user.entity'
+
+export class CreateReviewDto {
+  @IsUUID()
+  menuItem: UserEntity
+
+  @Length(3, 50)
+  title: string
+
+  @Length(3, 200)
+  comment: string
+
+  @Length(3, 300)
+  @IsOptional()
+  image?: string
+
+  @Min(1)
+  @Max(5)
+  ratings: number
+}
