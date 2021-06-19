1c3c8c4316214676df6ebc342a945d1865892b1b
developedBySJ <developedbysj@gmail.com>
2021-06-19 17:16:54 +0530

    Add reviews entity

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,43 @@
-export class Review {}
+import { Length, Max, Min } from 'class-validator'
+import { MenuItemEntity } from 'src/menu-items/entities/menu-item.entity'
+import { UserEntity } from 'src/users/entities/user.entity'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+
+@Entity('reviews')
+export class ReviewEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @ManyToOne(() => UserEntity, { onDelete: 'CASCADE' })
+  user: UserEntity
+
+  @ManyToOne(() => MenuItemEntity, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'menu_item' })
+  menuItem: UserEntity
+
+  @Length(3, 50)
+  @Column()
+  title: string
+
+  @Length(3, 200)
+  @Column()
+  comment: string
+
+  @Column({ nullable: true })
+  image?: string
+
+  @Min(1)
+  @Max(5)
+  @Column('integer')
+  ratings: number
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date
+}
