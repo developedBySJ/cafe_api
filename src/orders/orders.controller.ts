594d0d889eb93ada995caa504e64205850c3f689
developedBySJ <developedbysj@gmail.com>
2021-06-21 21:20:31 +0530

    Add order controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -6,37 +6,58 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common'
 import { OrdersService } from './orders.service'
 import { CreateOrderDto } from './dto/create-order.dto'
 import { UpdateOrderDto } from './dto/update-order.dto'
+import { JwtAuthGuard } from 'src/auth/guards'
+import { User } from 'src/common/decorators/user.decorator'
+import { UserEntity } from 'src/users/entities/user.entity'
+import { RolesGuard } from 'src/common/guards/roles.guards'
+import { Roles } from 'src/common/decorators'
+import { UserRole } from 'src/common'
 
 @Controller('orders')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto)
+  create(@User() user: UserEntity, @Body() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto, user)
   }
 
   @Get()
+  findAllByUser(@User() user: UserEntity) {
+    return this.ordersService.findAllByUser(user)
+  }
+
+  @Get('/all')
+  @Roles(UserRole.Admin)
   findAll() {
     return this.ordersService.findAll()
   }
 
+  @Get('/pending')
+  findPending() {
+    return this.ordersService.findAllPending()
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id)
+    return this.ordersService.findOne(id)
   }
 
   @Patch(':id')
+  @Roles(UserRole.Admin)
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto)
+    return this.ordersService.update(id, updateOrderDto)
   }
 
   @Delete(':id')
+  @Roles(UserRole.Admin)
   remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id)
+    return this.ordersService.remove(id)
   }
 }
