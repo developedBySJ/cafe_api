8fd9416f909f758b863db315bbf96d1a5f5977fc
developedBySJ <developedbysj@gmail.com>
2021-06-21 21:22:07 +0530

    Add order service

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { OrdersService } from './orders.service'
 import { OrdersController } from './orders.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { OrderEntity } from './entities/order.entity'
+import { UserItemsModule } from 'src/user-items/user-items.module'
+import { UserItemsService } from 'src/user-items/user-items.service'
+import { UserItemEntity } from 'src/user-items/entities/user-item.entity'
 
 @Module({
+  imports: [
+    UserItemsModule,
+    TypeOrmModule.forFeature([OrderEntity, UserItemEntity]),
+  ],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, UserItemsService],
 })
 export class OrdersModule {}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,110 @@
-import { Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserRole } from 'src/common'
+import { UserItemsService } from 'src/user-items/user-items.service'
+import { UserEntity } from 'src/users/entities/user.entity'
+import { LessThan, Repository } from 'typeorm'
+
 import { CreateOrderDto } from './dto/create-order.dto'
 import { UpdateOrderDto } from './dto/update-order.dto'
+import { OrderEntity, OrderStatus } from './entities/order.entity'
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order'
+  constructor(
+    private readonly _userItemsService: UserItemsService,
+    @InjectRepository(OrderEntity)
+    private readonly _orderRepository: Repository<OrderEntity>,
+  ) {}
+  async create(createOrderDto: CreateOrderDto, user: UserEntity) {
+    const cartItems = await this._userItemsService.findAll('cart', user)
+
+    if (!cartItems.length)
+      throw new BadRequestException('Add At least one product')
+
+    const total = cartItems.reduce(
+      (acc, cur) => acc + cur.qty * cur.menuItem.price,
+      0,
+    )
+
+    if (total !== createOrderDto.total)
+      throw new BadRequestException('order total not matching')
+
+    const deliveredAt = createOrderDto.isDelivered ? new Date() : null
+    const status =
+      user.role === UserRole.Customer
+        ? OrderStatus.Placed
+        : createOrderDto.status
+
+    const _newOrder = this._orderRepository.create({
+      ...createOrderDto,
+      user,
+      total,
+      orderItems: cartItems,
+      deliveredAt,
+      status,
+    })
+
+    const newOrder = await this._orderRepository.save(_newOrder)
+
+    await this._userItemsService.removeAll('cart', user)
+
+    return newOrder
   }
 
-  findAll() {
-    return `This action returns all orders`
+  async findAllByUser(user: UserEntity) {
+    const orders = await this._orderRepository.find({ user })
+    return orders
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`
+  async findAll() {
+    const orders = await this._orderRepository.find()
+    return orders
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`
+  async findAllPending() {
+    const orders = await this._orderRepository.find({
+      status: LessThan(OrderStatus.Delivered),
+    })
+    return orders
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`
+  async findOne(id: string) {
+    const order = await this._orderRepository.findOne(id)
+
+    if (!order) throw new NotFoundException()
+
+    return order
+  }
+
+  async update(id: string, { status, isDelivered }: UpdateOrderDto) {
+    const order = await this.findOne(id)
+
+    if (![OrderStatus.Cancelled, OrderStatus.Delivered].includes(order.status))
+      order.status = status
+
+    if (
+      status === OrderStatus.Delivered ||
+      (!order.deliveredAt && isDelivered)
+    ) {
+      order.deliveredAt = new Date()
+      order.status = OrderStatus.Delivered
+    }
+
+    const updatedOrder = await this._orderRepository.save(order)
+
+    return updatedOrder
+  }
+
+  async remove(id: string) {
+    const order = await this.findOne(id)
+
+    await this._orderRepository.softRemove(order)
+
+    return null
   }
 }
