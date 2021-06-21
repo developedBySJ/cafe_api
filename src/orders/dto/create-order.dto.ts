86baa677e741180f7a9e7cd56cdc9bc11151f612
developedBySJ <developedbysj@gmail.com>
2021-06-21 21:21:25 +0530

    Add create and update order dtos

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,41 @@
-export class CreateOrderDto {}
+import {
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsUUID,
+  Length,
+  Max,
+  Min,
+} from 'class-validator'
+import { OrderStatus } from '../entities/order.entity'
+
+class OrderItems {
+  @IsUUID()
+  id: string
+
+  @Min(1)
+  @Max(10)
+  qty: number
+}
+
+export class CreateOrderDto {
+  @IsInt()
+  @IsOptional()
+  table?: string
+
+  @Min(1)
+  total: number
+
+  @IsEnum(OrderStatus)
+  @IsOptional()
+  status: OrderStatus = OrderStatus.Placed
+
+  @Length(3, 100)
+  @IsOptional()
+  notes?: string
+
+  @IsBoolean()
+  @IsOptional()
+  isDelivered?: boolean = false
+}
--- src/orders/dto/update-order.dto.ts
+++ src/orders/dto/update-order.dto.ts
@@ -1,4 +1,7 @@
-import { PartialType } from '@nestjs/swagger'
+import { PickType } from '@nestjs/swagger'
 import { CreateOrderDto } from './create-order.dto'
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto extends PickType(CreateOrderDto, [
+  'isDelivered',
+  'status',
+]) {}
