4c939a294c08623b83e56d3e1073c370e4a475bb
developedBySJ <developedbysj@gmail.com>
2021-06-27 19:04:58 +0530

    Add menu filter dto

---
+++ src/menus/dto/menu-filter.dto.ts
@@ -0,0 +1,14 @@
+import { Transform } from 'class-transformer'
+import { IsBoolean, IsOptional, IsString } from 'class-validator'
+import { PageOptionsDto } from 'src/common/dto/page-options.dto'
+
+export class MenuFilters extends PageOptionsDto {
+  @IsString()
+  @IsOptional()
+  search?: string
+
+  @IsBoolean()
+  @Transform(({ value }) => ['1', 1, 'true', true].includes(value))
+  @IsOptional()
+  isActive?: boolean
+}
