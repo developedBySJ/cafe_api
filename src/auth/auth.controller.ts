cc7f763ae90f5e7c1527089892764eec625833ca
developedBySJ <developedbysj@gmail.com>
2021-06-28 18:55:26 +0530

    Add refresh token endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,16 +12,20 @@ import {
   Param,
 } from '@nestjs/common'
 import { Response, Request } from 'express'
+import { User } from 'src/common/decorators/user.decorator'
 import { CreateUserDto } from 'src/users/dto/create-user.dto'
+import { UserEntity } from 'src/users/entities/user.entity'
 import { AuthService } from './auth.service'
 import { ForgotPasswordDto } from './dto/forgot-password.dto'
 import { ResetPassword } from './dto/reset-password.dto'
-import { LocalAuthGuard } from './guards'
+import { JwtAuthGuard, LocalAuthGuard } from './guards'
+import JwtRefreshGuard from './guards/refresh.guard'
 
 @Controller('/')
 export class AuthController {
   constructor(private readonly _authService: AuthService) {}
   @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAuthGuard)
   @Patch('/logout')
   logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     return this._authService.logout(req, res)
@@ -55,5 +59,14 @@ export class AuthController {
     @Body() { password }: ResetPassword,
   ) {
     return this._authService.resetPassword(param, password)
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('refresh')
+  async refresh(
+    @Res({ passthrough: true }) res: Response,
+    @User() user: UserEntity,
+  ) {
+    await this._authService._setRefreshTokenCookie(res, user.id)
+    return user
   }
 }
