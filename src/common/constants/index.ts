ce963125451487765c535ac3e526e4bf6c193ac2
developedBySJ <developedbysj@gmail.com>
2021-06-28 18:50:00 +0530

    Add refresh token cookie key

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,4 +3,5 @@ import { AssetType, UserRole } from '../types'
 export * from './env.constant'
 
 export const AUTH_COOKIE_KEY = '__auth'
+export const REFRESH_TOKEN_COOKIE_KEY = '__refresh'
 export const CLOUDINARY_FOLDER = 'cafe'
