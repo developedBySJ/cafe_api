9a1590fa5ca3a00c8d8d383986519556e5f73fc9
developedBySJ <developedbysj@gmail.com>
2021-07-05 18:32:28 +0530

    Add unique name index Inventory entity

--- src/inventory/entities/inventory.entity.ts
+++ src/inventory/entities/inventory.entity.ts
@@ -14,7 +14,7 @@ export class InventoryEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @Column()
+  @Column({ unique: true })
   name: string
 
   @Column({ name: 'available_stock' })
