669265d86b968871569ddefe4075dcce6f77ec61
developedBySJ <developedbysj@gmail.com>
2021-07-06 18:20:42 +0530

    Add cors and helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { NestFactory, Reflector } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import * as helmet from 'helmet'
 import { AppModule } from './app.module'
 import { setUpSwagger } from './utils'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.use(helmet())
+  app.enableCors()
   app.use(cookieParser())
+
   app.setGlobalPrefix('/api/v1')
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
