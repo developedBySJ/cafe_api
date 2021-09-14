a46b396fda2963e26dca76e9f021d0dd0367f876
developedBySJ <developedbysj@gmail.com>
2021-09-14 15:38:58 +0530

    Add https redirect

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { NestFactory, Reflector } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import * as helmet from 'helmet'
+import sslRedirect from 'heroku-ssl-redirect'
 import { AppModule } from './app.module'
 import { setUpSwagger } from './utils'
 
+console.log(process.env)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.use(helmet({ contentSecurityPolicy: false }))
-  // app.enableCors()
+  app.use(sslRedirect())
+  app.enableCors()
   app.use(cookieParser())
-
   app.setGlobalPrefix('/api/v1')
 
   app.useGlobalPipes(
